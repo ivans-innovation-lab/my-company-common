18ba88da22f6a9ed102e5a00c1052774fae9a99d
Ivan Dugalic <ivan.dugalic@namics.com>
2017-10-29 16:27:56 +0100

    'MemberRemovedFromTeam' changed

--- my-company-common-team/src/main/java/com/idugalic/common/team/event/MemberRemovedFromTeamEvent.java
+++ my-company-common-team/src/main/java/com/idugalic/common/team/event/MemberRemovedFromTeamEvent.java
@@ -2,23 +2,22 @@ package com.idugalic.common.team.event;
 
 import com.idugalic.common.event.AuditableAbstractEvent;
 import com.idugalic.common.model.AuditEntry;
-import com.idugalic.common.team.model.Member;
 
 public class MemberRemovedFromTeamEvent extends AuditableAbstractEvent {
 
     private static final long serialVersionUID = 8894380375023480978L;
-    private Member member;
+    private String memberId;
    
     public MemberRemovedFromTeamEvent() {
     }
 
-    public MemberRemovedFromTeamEvent(String id, AuditEntry auditEntry, Member member) {
+    public MemberRemovedFromTeamEvent(String id, AuditEntry auditEntry, String memberId) {
         super(id, auditEntry);
-        this.member = member;
+        this.memberId = memberId;
     }
 
-	public Member getMember() {
-		return member;
+	public String getMemberId() {
+		return memberId;
 	}
-    
+
 }
